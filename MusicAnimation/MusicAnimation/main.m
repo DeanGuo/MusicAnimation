a684eda8235687af40b9bec01b31db11522b0646
Guo Tianlong <guo.tianlong@qq.com>
2014-12-26 15:26:58 +0800

    empty project

---
+++ MusicAnimation/MusicAnimation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicAnimation
+//
+//  Created by 郭天龙 on 12/26/14.
+//  Copyright (c) 2014 nice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
